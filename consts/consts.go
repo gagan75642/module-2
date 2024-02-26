c5246e2ccd36205469ce3849ab19d1d16811696f
gagan75642 <160026133+gagan75642@users.noreply.github.com>
2024-02-26 23:30:26 +0530

    Update consts.go

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "gagankumar"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "Gagan"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "GN"
 )
 
 var ID ids.ID
